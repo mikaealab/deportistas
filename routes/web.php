f713d1c3eb2e7324c1c59b764b70f3c44ee9a387
mikaelab <nashely.betancourt6156@utc.edu.ec>
2025-12-02 01:06:35 -0500

    registro de la rout deportista

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaisController;
 use App\Http\Controllers\DisciplinaController;
+use App\Http\Controllers\DeportistaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,4 @@ Route::resource('paises', PaisController::class);
 
 Route::resource('disciplina', DisciplinaController::class);
 
+Route::resource('deportista', DeportistaController::class);
