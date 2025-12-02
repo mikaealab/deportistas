cdb832a3a9670c84e205ae66b2c0a1b02a3c54a4
mikaelab <nashely.betancourt6156@utc.edu.ec>
2025-12-02 00:42:58 -0500

    edicion del modelo disciplina

--- app/Models/Disciplina.php
+++ app/Models/Disciplina.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Disciplina extends Model
 {
     //
+    use HasFactory;
+    protected $table = 'disciplina';
+    protected $primaryKey = 'id_disciplina';
+    protected $fillable = ['disciplina'];
+    public $timestamps = false;
 }
