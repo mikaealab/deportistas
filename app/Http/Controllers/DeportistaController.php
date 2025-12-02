416f9c59183ecac41fef995a6c1a0fea6b6ee85a
mikaelab <nashely.betancourt6156@utc.edu.ec>
2025-12-02 01:05:50 -0500

    edicion del controlador deportista

--- app/Http/Controllers/DeportistaController.php
+++ app/Http/Controllers/DeportistaController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Deportista;
+
 class DeportistaController extends Controller
 {
     /**
@@ -13,6 +15,8 @@ class DeportistaController extends Controller
     public function index()
     {
         //
+        $deportistas = Deportista::all();
+        return view('deportista.index', compact('deportistas'));
     }
 
     /**
@@ -21,6 +25,7 @@ class DeportistaController extends Controller
     public function create()
     {
         //
+        return view('deportista.nuevo');
     }
 
     /**
@@ -29,6 +34,16 @@ class DeportistaController extends Controller
     public function store(Request $request)
     {
         //
+        $deportista = [
+            'nombre' => $request->nombre,
+            'fecha_nacimiento' => $request->fecha_nacimiento,
+            'estatura_cm' => $request->estatura_cm,
+            'peso_kg' => $request->peso_kg,
+            'id_pais' => $request->id_pais,
+            'id_disciplina' => $request->id_disciplina
+        ];
+        Deportista::create($deportista);
+        return redirect()->route('deportista.index')->with('success', 'El deportista ha sido creado');
     }
 
     /**
@@ -45,6 +60,8 @@ class DeportistaController extends Controller
     public function edit(string $id)
     {
         //
+        $deportista = Deportista::find($id);
+        return view('deportista.editar', compact('deportista'));
     }
 
     /**
@@ -53,6 +70,15 @@ class DeportistaController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $deportista = Deportista::find($id);
+        $deportista->nombre = $request->nombre;
+        $deportista->fecha_nacimiento = $request->fecha_nacimiento;
+        $deportista->estatura_cm = $request->estatura_cm;
+        $deportista->peso_kg = $request->peso_kg;
+        $deportista->id_pais = $request->id_pais;
+        $deportista->id_disciplina = $request->id_disciplina;
+        $deportista->save();
+        return redirect()->route('deportista.index')->with('success', 'El deportista ha sido actualizado');
     }
 
     /**
@@ -61,5 +87,8 @@ class DeportistaController extends Controller
     public function destroy(string $id)
     {
         //
+        $deportista = Deportista::find($id);
+        $deportista->delete();
+        return redirect()->route('deportista.index')->with('success', 'El deportista ha sido eliminado');
     }
 }
