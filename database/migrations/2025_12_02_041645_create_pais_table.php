1e85b17e73cb2f2ea1b28a199669eff0b10c5d96
mikaelab <nashely.betancourt6156@utc.edu.ec>
2025-12-01 23:24:21 -0500

    migracion Pais

--- database/migrations/2025_12_02_041645_create_pais_table.php
+++ database/migrations/2025_12_02_041645_create_pais_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('pais', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->strig("pais");
         });
     }
 
