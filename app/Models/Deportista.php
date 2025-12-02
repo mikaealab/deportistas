e6fd87b95c4581a9476d63e11ea50fa74129edd2
mikaelab <nashely.betancourt6156@utc.edu.ec>
2025-12-02 01:03:46 -0500

    edicion del modelo deportista

--- app/Models/Deportista.php
+++ app/Models/Deportista.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Deportista extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'deportista';
+    protected $primaryKey = 'id_deportista';
+    protected $fillable = [
+        'nombre',
+        'fecha_nacimiento',
+        'estatura_cm',
+        'peso_kg',
+        'id_pais',
+        'id_disciplina'
+    ];
+    public $timestamps = false;
+    // Relaciones
+    public function pais()
+    {
+        return $this->belongsTo(Pais::class);
+    }
+    public function disciplina()
+    {
+        return $this->belongsTo(Disciplina::class);
+    }
 }
