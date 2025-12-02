c473b6e86f848eea63084d827de5abeb2cc289e8
mikaelab <nashely.betancourt6156@utc.edu.ec>
2025-12-02 00:46:29 -0500

    registro de la rout disciplina

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaisController;
+use App\Http\Controllers\DisciplinaController;
 
-Route::get('/', [PaisController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('paises', PaisController::class);
+
+Route::resource('disciplina', DisciplinaController::class);
+
